b89d30a35e4a0e7c1aef80e16cdd05fb98821f63
João Henrique <jhbbortolotto@gmail.com>
2019-08-11 15:22:37 -0300

    Testing classes.

---
+++ domain/src/test/kotlin/com/jhbb/domain/interactor/bookmarked/GetBookmarkedProjectsTest.kt
@@ -0,0 +1,48 @@
+package com.jhbb.domain.interactor.bookmarked
+
+import com.jhbb.domain.executor.PostExecutionThread
+import com.jhbb.domain.model.Project
+import com.jhbb.domain.repository.ProjectRepository
+import com.jhbb.domain.test.ProjectDataFactory
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Observable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.util.*
+
+class GetBookmarkedProjectsTest {
+
+    private lateinit var getBookmarkedProjects: GetBookmarkedProjects
+    @Mock lateinit var projectRepository: ProjectRepository
+    @Mock lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        getBookmarkedProjects = GetBookmarkedProjects(projectRepository, postExecutionThread)
+    }
+
+    @Test
+    fun getBookmarkedProjectsComplete() {
+        stubGetProjects(Observable.just(ProjectDataFactory.makeProjectList(2)))
+        val testObserver = getBookmarkedProjects.buildUseCaseObservable().test()
+        testObserver.assertComplete()
+    }
+
+    @Test
+    fun getBookmarkedProjectsReturnsData() {
+        val bookmarkedProjects = ProjectDataFactory.makeProjectList(2)
+        stubGetProjects(Observable.just(bookmarkedProjects))
+
+        val testObserver = getBookmarkedProjects.buildUseCaseObservable().test()
+
+        testObserver.assertValue(bookmarkedProjects)
+    }
+
+    private fun stubGetProjects(observable: Observable<List<Project>>) {
+        whenever(projectRepository.getBookmarkedProjects())
+            .thenReturn(observable)
+    }
+}
---
+++ domain/src/test/kotlin/com/jhbb/domain/interactor/bookmarked/UnbookmarkProjectTest.kt
@@ -0,0 +1,47 @@
+package com.jhbb.domain.interactor.bookmarked
+
+import com.jhbb.domain.executor.PostExecutionThread
+import com.jhbb.domain.repository.ProjectRepository
+import com.jhbb.domain.test.ProjectDataFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.lang.IllegalArgumentException
+
+class UnbookmarkProjectTest {
+
+    private lateinit var unbookmarkProject: UnbookmarkProject
+    @Mock lateinit var projectRepository: ProjectRepository
+    @Mock lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        unbookmarkProject = UnbookmarkProject(projectRepository, postExecutionThread)
+    }
+
+    @Test
+    fun unbookmarkProjectCompletes() {
+        stubUnbookmarkProject(Completable.complete())
+
+        val testObserver = unbookmarkProject.buildUseCaseCompletable(
+            UnbookmarkProject.Params.forProject(ProjectDataFactory.randomUuid())
+        ).test()
+
+        testObserver.assertComplete()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun unbookmarkProjectThrowsException() {
+        val testObserver = unbookmarkProject.buildUseCaseCompletable().test()
+    }
+
+    private fun stubUnbookmarkProject(completable: Completable) {
+        whenever(projectRepository.unbookmarkProject(any()))
+            .thenReturn(completable)
+    }
+}
